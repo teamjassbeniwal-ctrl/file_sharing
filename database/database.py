db55165dc6f90b2c5278405a714605ea569ddf46
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2025-12-25 16:58:52 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -19,7 +19,7 @@ def new_user(id):
         '_id': id,
         'verify_status': {
             'is_verified': False,
-            'verified_time': "",
+            'verified_time': 0,
             'verify_token': "",
             'link': ""
         }
@@ -32,9 +32,8 @@ async def present_user(user_id: int):
 async def add_user(user_id: int):
     user = new_user(user_id)
     await user_data.insert_one(user)
-    return
 
-async def db_verify_status(user_id):
+async def db_verify_status(user_id: int):
     user = await user_data.find_one({'_id': user_id})
     if user:
         return user.get('verify_status', default_verify)
@@ -45,9 +44,8 @@ async def db_update_verify_status(user_id, verify):
 
 async def full_userbase():
     user_docs = user_data.find()
-    user_ids = [doc['_id'] async for doc in user_docs]
-    return user_ids
+    user_ids = await user_docs.to_list(length=None)
+    return [doc['_id'] for doc in user_ids]
 
 async def del_user(user_id: int):
     await user_data.delete_one({'_id': user_id})
-    return
