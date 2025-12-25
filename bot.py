5b6cb7a40436bdeb91667b496fe34d651f2f18fa
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2025-12-25 11:19:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ultroid_official for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/allbotsupdates1 for support")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -55,13 +55,13 @@ class Bot(Client):
             self.LOGGER(__name__).warning(f"Error occurred: {e}")
             self.LOGGER(__name__).warning(f"CHANNEL_ID: {CHANNEL_ID}, DB Channel ID: {db_channel.id if 'db_channel' in locals() else 'N/A'}")
             self.LOGGER(__name__).warning(f"Make sure bot is Admin in DB Channel, and Double-check the CHANNEL_ID value.")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ultroid_official for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/allbotsupdates1 for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ultroid_official")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/allbotsupdates1")
         self.LOGGER(__name__).info(f""" \n\n       
-(っ◔◡◔)っ ♥ ULTROIDOFFICIAL ♥
+(っ◔◡◔)っ ♥ Teqm JB ♥
 ░╚════╝░░╚════╝░╚═════╝░╚══════╝
                                           """)
         self.username = usr_bot_me.username
