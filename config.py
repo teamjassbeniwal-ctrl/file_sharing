e5d84b5f15491c48ce2048d2bbde908137cf5703
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2025-12-25 16:59:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,99 +2,79 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-#Bot token @Botfather
+# Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-#Your API ID & API HASH from my.telegram.org 
-#Your API ID from my.telegram.org
+# API ID & Hash
 APP_ID = int(os.environ.get("APP_ID", "31212516"))
-
-#Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "ccc20df465364045538da8ecf8954992l")
 
-#Your db channel Id
+# DB Channel ID
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001925121665"))
 
-#OWNER ID
+# Owner ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "6645288126"))
 
-#Port
+# Port
 PORT = os.environ.get("PORT", "8585")
 
-#Database 
-#Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ultroidxTeam:ultroidxTeam@cluster0.gabxs6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# MongoDB
+DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
-#Shortner (token system) 
-# check my discription to help by using my refer link of shareus.io
-# 
-
+# Shortlink system
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "arolinks.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "8d6b2153682ca17a4f89e662199d6b5917893bf0")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/+JAdctcMYdSUzZGU1") # shareus ka tut_vid he 
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 21600))  # 6 hours in seconds
+IS_VERIFY = os.environ.get("IS_VERIFY", "True") == "True"
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/+JAdctcMYdSUzZGU1")
 
 # Auto-delete configuration
-AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "180"))
+AUTO_DELETE_TIME = int(os.environ.get("AUTO_DELETE_TIME", "180"))
 AUTO_DELETE_MSG = os.environ.get(
     "AUTO_DELETE_MSG",
-    "This file will be automatically deleted in {time} seconds. Please ensure you have saved any necessary content before this time."
+    "This file will be automatically deleted in {time} seconds. Please save it."
 )
 AUTO_DEL_SUCCESS_MSG = os.environ.get(
     "AUTO_DEL_SUCCESS_MSG",
-    "Your file has been successfully deleted. Thank you for using our service. ✅"
+    "Your file has been successfully deleted. ✅"
 )
-#force sub channel id, if you want enable force sub
+
+# Force subscription
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1003598465147"))
+FORCE_MSG = os.environ.get(
+    "FORCE_SUB_MESSAGE",
+    "Hello {first}, please join my Channel to use the bot!"
+)
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+# Bot behavior
+START_MSG = os.environ.get(
+    "START_MESSAGE",
+    "Hello {first}!\nI can store your files securely."
+)
+CUSTOM_CAPTION = os.environ.get(
+    "CUSTOM_CAPTION",
+    "This content is from the internet. We are not the original creator."
+)
+PROTECT_CONTENT = os.environ.get("PROTECT_CONTENT", "False") == "True"
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", "False") == "True"
 
-#start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1480923991 5069922547 6695586027").split()):
-        ADMINS.append(int(x))
-except ValueError: 
-        raise Exception("Your Admins list does not contain valid integers.")
-
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
-
-#set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We LeakHubd or its subsidiary channel doesn't produce any of them.")
-
-#set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
-
-#Set true if you want Disable your Channel Posts Share button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
-
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
-
+# Admins
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "1480923991 5069922547 6695586027").split()]
 ADMINS.append(OWNER_ID)
-ADMINS.append(1955406483)
 
+# Logging
 LOG_FILE_NAME = "filesharingbot.txt"
-
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt='%d-%b-%y %H:%M:%S',
     handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME,
-            maxBytes=50000000,
-            backupCount=10
-        ),
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
         logging.StreamHandler()
     ]
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
